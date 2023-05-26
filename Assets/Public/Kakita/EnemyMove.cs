ce27d88e59f6b653fe43e102ba97417ed8752495
skakita7 <s.kakita717@gmail.com>
2023-05-26 14:18:08 +0900

    敵の動き付けました

---
+++ Assets/Public/Kakita/EnemyMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField] float speed;
+    private GameObject playerObject;
+    private Rigidbody2D enemyRb;
+    private Vector3 PlayerPosition;
+    private Vector3 EnemyPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GameObject.FindWithTag("Player");
+        enemyRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRb.AddForce(playerObject.transform.position - transform.position);
+    }
+}
